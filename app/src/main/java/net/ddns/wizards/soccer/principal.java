5a7683ac8572cc77c3c7ccbf27f17daa6bbcaabc
Miguel Pons <nickpons666@hotmail.com>
2017-04-07 15:16:00 -0500

    Inicio

---
+++ app/src/main/java/net/ddns/wizards/soccer/principal.java
@@ -0,0 +1,13 @@
+package net.ddns.wizards.soccer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
